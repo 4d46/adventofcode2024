16add251a41873b24fece2f9113688fc74bb49be
4d46 <dev@4d46.uk>
2024-12-24 07:43:38 +0000

    Starting to work on Part 2

--- day15/main.go
+++ day15/main.go
@@ -25,9 +25,13 @@ const (
 )
 
 const (
-	Wall  = '#'
-	Box   = 'O'
-	Robot = '@'
+	Wall      = '#'
+	Box       = 'O'
+	Robot     = '@'
+	Space     = '.'
+	DoubleBox = "[]"
+	BoxL      = '['
+	BoxR      = ']'
 )
 
 func main() {
@@ -35,60 +39,198 @@ func main() {
 
 	// Part 1
 	// Load the input data
-	inputData := LoadInputData("input1.txt")
+	// inputData := LoadInputData("input1.txt")
 	// inputData := LoadInputData("test1.txt")
 	// inputData := LoadInputData("test2.txt")
-
-	// The strings in the input data are a warehouse layout
-	// Convert the strings to a 2D slice of runes
-	var warehouse [][]rune
-	var moveInstructions []Direction
-	for _, line := range inputData {
-		if len(line) < 1 {
-			continue
-		} else if line[0] == Wall {
-			warehouse = append(warehouse, []rune(line))
-		} else {
-			// This must be a set of move instructions
-			// Loop over the string and convert to directions
-			for _, r := range line {
-				switch r {
-				case '^':
-					moveInstructions = append(moveInstructions, Up)
-				case '>':
-					moveInstructions = append(moveInstructions, Right)
-				case 'v':
-					moveInstructions = append(moveInstructions, Down)
-				case '<':
-					moveInstructions = append(moveInstructions, Left)
-				default:
-					log.Fatalf("Invalid move instruction: %c", r)
+	inputData := LoadInputData("test3.txt")
+	{
+		// The strings in the input data are a warehouse layout
+		// Convert the strings to a 2D slice of runes
+		var warehouse [][]rune
+		var moveInstructions []Direction
+		for _, line := range inputData {
+			if len(line) < 1 {
+				continue
+			} else if line[0] == Wall {
+				warehouse = append(warehouse, []rune(line))
+			} else {
+				// This must be a set of move instructions
+				// Loop over the string and convert to directions
+				for _, r := range line {
+					switch r {
+					case '^':
+						moveInstructions = append(moveInstructions, Up)
+					case '>':
+						moveInstructions = append(moveInstructions, Right)
+					case 'v':
+						moveInstructions = append(moveInstructions, Down)
+					case '<':
+						moveInstructions = append(moveInstructions, Left)
+					default:
+						log.Fatalf("Invalid move instruction: %c", r)
+					}
 				}
 			}
 		}
-	}
-	printWarehouse(warehouse)
+		printWarehouse(warehouse)
 
-	// Find the starting position of the robot
-	var robotPos Position
-	for y, row := range warehouse {
-		for x, r := range row {
-			if r == Robot {
-				robotPos = Position{x, y}
-				break
+		// Find the starting position of the robot
+		var robotPos Position
+		for y, row := range warehouse {
+			for x, r := range row {
+				if r == Robot {
+					robotPos = Position{x, y}
+					break
+				}
 			}
 		}
-	}
-	fmt.Printf("Robot starting position: %v\n", robotPos)
+		fmt.Printf("Robot starting position: %v\n", robotPos)
 
-	// Move the robot in the specified directions
-	for _, dir := range moveInstructions {
-		robotPos = moveRobot(warehouse, robotPos, dir)
-		// fmt.Printf("--------------------\n")
-		// printWarehouse(warehouse)
+		// Move the robot in the specified directions
+		for _, dir := range moveInstructions {
+			robotPos = moveRobot(warehouse, robotPos, dir)
+			// fmt.Printf("--------------------\n")
+			// printWarehouse(warehouse)
+		}
+		printWarehouse(warehouse)
+		fmt.Printf("Part 1: %d\n", sumBoxLocations(warehouse))
 	}
-	printWarehouse(warehouse)
-	fmt.Printf("Part 1: %d\n", sumBoxLocations(warehouse))
+	fmt.Println("--------------------")
+	{
+		// Part 2
+		// Load the input data, but this time the map should be created with walls, boxes and space twice as wide as the original map
+		// inputData := LoadInputData("input2.txt")
+		var warehouse [][]rune
+		var moveInstructions []Direction
+		for _, line := range inputData {
+			if len(line) < 1 {
+				continue
+			} else if line[0] == Wall {
+				doubleWidth := makeMapDoubleWidth(line)
+				warehouse = append(warehouse, doubleWidth)
+			} else {
+				// This must be a set of move instructions
+				// Loop over the string and convert to directions
+				for _, r := range line {
+					switch r {
+					case '^':
+						moveInstructions = append(moveInstructions, Up)
+					case '>':
+						moveInstructions = append(moveInstructions, Right)
+					case 'v':
+						moveInstructions = append(moveInstructions, Down)
+					case '<':
+						moveInstructions = append(moveInstructions, Left)
+					default:
+						log.Fatalf("Invalid move instruction: %c", r)
+					}
+				}
+			}
+		}
+		printWarehouse(warehouse)
+
+		// Find the starting position of the robot
+		var robotPos Position
+		for y, row := range warehouse {
+			for x, r := range row {
+				if r == Robot {
+					robotPos = Position{x, y}
+					break
+				}
+			}
+		}
+		fmt.Printf("Robot starting position: %v\n", robotPos)
+
+		// Move the robot in the specified directions, but this time boxes are twice as wide and consist of 2 characters "[]"
+		for _, dir := range moveInstructions {
+			robotPos = moveRobotWide(warehouse, robotPos, dir)
+			fmt.Printf("--------------------\n")
+			printWarehouse(warehouse)
+		}
+	}
+}
+
+// Create a new map that is twice as wide as the original map
+func makeMapDoubleWidth(line string) []rune {
+	doubleWidth := make([]rune, 0)
+	for _, r := range line {
+		switch r {
+		case Wall:
+			doubleWidth = append(doubleWidth, Wall, Wall)
+		case Box:
+			doubleWidth = append(doubleWidth, '[', ']')
+		case Robot:
+			doubleWidth = append(doubleWidth, Robot, '.')
+		case Space:
+			doubleWidth = append(doubleWidth, Space, Space)
+		default:
+			log.Fatalf("Invalid character in map: %c", r)
+		}
+	}
+	return doubleWidth
+}
+
+// Attempt to move the robot in the specificed direction in the warehouse.  If the robot impacts a Box then if there is an available space
+// in the direction of movement, the Box will be moved in that direction.  Do this recursively until the robot is able to move in the
+// specified direction. If the robot is unable to move in the specified direction, return that same position, otherwise return the new position
+// But boxes ar now twice as wide and consist of 2 characters "[]", Boxes are moved as a single unit and boxes that overlap at all will be moved
+// as a single unit
+func moveRobotWide(warehouse [][]rune, robotPos Position, dir Direction) Position {
+	// Get the new position
+	newPos := iteratePosition(robotPos, dir)
+
+	// Check if the new position is a wall
+	if warehouse[newPos.y][newPos.x] == Wall {
+		return robotPos
+	}
+
+	// Check if the new position is a Box
+	if warehouse[newPos.y][newPos.x] == BoxL || warehouse[newPos.y][newPos.x] == BoxR {
+		// Attempt to move the Box
+		boxPos := moveBoxWide(warehouse, newPos, dir)
+		// If the Box was unable to move, return the current position
+		if boxPos == newPos {
+			return robotPos
+		}
+	}
+
+	// Move the robot to the new position
+	warehouse[robotPos.y][robotPos.x] = '.'
+	warehouse[newPos.y][newPos.x] = Robot
+	return newPos
+}
+
+// Move the Box in the specified direction.  If the Box is unable to move in the specified direction, return the current position, otherwise
+// return the new position
+// But boxes ar now twice as wide and consist of 2 characters "[]", Boxes are moved as a single unit and boxes that overlap at all will be moved
+// as a single unit
+func moveBoxWide(warehouse [][]rune, boxPos Position, dir Direction) Position {
+	// Get the 2 new positions the box will occupy
+	newPosL := iteratePosition(boxPos, dir)
+	newPosR := iteratePosition(newPosL, dir)
+
+	// Check if either new position is a wall
+	if warehouse[newPosL.y][newPosL.x] == Wall || warehouse[newPosR.y][newPosR.x] == Wall {
+		return boxPos
+	}
+
+	// Check if either new position is a Box
+	if warehouse[newPosL.y][newPosL.x] == BoxL || warehouse[newPosL.y][newPosL.x] == BoxR ||
+		warehouse[newPosR.y][newPosR.x] == BoxL || warehouse[newPosR.y][newPosR.x] == BoxR {
+		// Attempt to move the Box
+		newBoxPos := moveBoxWide(warehouse, newPosL, dir)
+		// If the Box was unable to move, return the current position
+		if newBoxPos == newPosL {
+			return boxPos
+		}
+	}
+
+	// Move the Box to the new position
+	warehouse[boxPos.y][boxPos.x] = '.'
+	warehouse[newPosL.y][newPosL.x] = BoxL
+	warehouse[newPosR.y][newPosR.x] = BoxR
+	return newPosL
+
 }
 
 // Attempt to move the robot in the specificed direction in the warehouse.  If the robot impacts a Box then if there is an available space
